a8d79f1e30b1d3fbc06e0b4d5a9df622947c5f07
h0han <hyhan@dgist.ac.kr>
2020-11-28 19:33:25 +0900

    Upload code

---
+++ code/SE271_project/SE271_project/header.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+#ifndef SE271_PROJECT
+#define SE271_PROJECT
+
+class Person {
+public:
+	std::string name;
+	std::string gender; // represent 0(male), 1(female)
+	std::string mail;
+	int* subject = new int[100]{ 0 };
+};
+
+class Student : public Person {
+public:
+	std::string id = "0"; // student number
+	std::string password;
+	std::string mbti;
+	std::string self_introduction;
+	double score = 0;
+	bool operator <(Student& student);
+};
+
+class Professor : public Person {
+public:
+	std::string officeloc;
+};
+
+void MakeObject(Student students[], std::string filename);
+void MakeProfessor(Professor[]);
+void Menuselect(Student students[], Professor[]);
+int toHash(std::string);
+
+#endif
---
+++ code/SE271_project/SE271_project/main.cpp
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int main() {
+
+	Student students[800];
+	Professor professors[120];
+	MakeObject(students, "student_database.csv"); // open csv and make objects(students)
+	MakeProfessor(professors); // open csv and make professors
+	Menuselect(students, professors);
+
+	return 0;
+}
