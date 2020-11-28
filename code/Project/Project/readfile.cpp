f521ff71d68178a9371ee8db36b18ea081577ec9
h0han <hyhan@dgist.ac.kr>
2020-11-28 14:45:48 +0900

    Upload on Nov 28

--- code/Project/Project/readfile.cpp
+++ code/Project/Project/readfile.cpp
@@ -1,29 +1,24 @@
-#include "SE271.h"
-#include <sstream>
-
-void open_csv(std::string filename) {
-    std::string str_buf;
-    std::fstream fs;
-    std::istringstream iss(str_buf);
-    std::string token;
-    std::string mbti_arr[32][32];
-    int i = 0; int j = 0; // for loop
-
-    fs.open(filename, std::ios::in);
-
-    while (!fs.eof()) {
-        getline(fs, str_buf);
-        std::istringstream iss(str_buf);
-        while (getline(iss, token, ',')) {
-            std::cout << token << std::endl;
-            mbti_arr[j][i] = token;
-            ++i;
-        } ++j;
-    }
-    for (int k = 0; k < 16; ++k) {
-        for (int l = 0; l < 16; ++l) {
-            std::cout << mbti_arr[k][l] << '\n';
-        }
-    } // 공백 발생하는 문제
-    fs.close();
-}
+//#include "SE271.h"
+//#include <sstream>
+//
+//void csv_to_arr(std::string filename) {
+//    std::string str_buf;
+//    std::fstream fs;
+//    std::istringstream iss(str_buf);
+//    std::string token;
+//    std::string mbti_arr[20][20];
+//    int i = 0; int j = 0; // for loop
+//
+//    fs.open(filename, std::ios::in);
+//
+//    while (!fs.eof()) {
+//        i = 0;
+//        getline(fs, str_buf);
+//        std::istringstream iss(str_buf);
+//        while (getline(iss, token, ',')) {
+//            mbti_arr[j][i] = token;
+//            ++i;
+//        } ++j;
+//    }
+//    fs.close();
+//}
