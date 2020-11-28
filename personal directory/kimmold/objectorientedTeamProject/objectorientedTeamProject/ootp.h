332f8c78f276e9d0cb20173f386b3a4ff490c3f3
kimjh5182 <kimjh5182@naver.com>
2020-11-28 15:47:27 +0900

    fffsf

--- personal directory/kimmold/objectorientedTeamProject/objectorientedTeamProject/ootp.h
+++ personal directory/kimmold/objectorientedTeamProject/objectorientedTeamProject/ootp.h
@@ -4,16 +4,23 @@
 #include<istream>
 #include<string>
 
-class Student {
+class Person {
+	std::string name;
+	std::string mail;
+	std::string mbti;
+	int* subject = new int[100]{ 0 };
+};
+
+class Student : public Person{
 public:
 	int id=0; // student number
 	int gender=-1; // represent 0(male), 1(female)
 	std::string password;
-	std::string name;
-	std::string mail;
-	std::string mbti;
-	int* subject = new int[100]{0};
 	std::string self_introduction;
 	//Student();
 	//~Student();
+};
+
+class Professor : public Person {
+	std::string officeloc;
 };
