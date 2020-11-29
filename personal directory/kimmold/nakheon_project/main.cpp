baef3c9fd47f1383305b8518f1dadb7ecdd8b052
kimjh5182 <kimjh5182@naver.com>
2020-11-29 22:22:11 +0900

    테스트테스트

---
+++ personal directory/kimmold/nakheon_project/main.cpp
@@ -0,0 +1,13 @@
+#include "nakon_header.h"
+
+int main() {
+	
+	Student students[800];
+	Professor professors[120];
+	open_csv_and_make_object(students, "student_database2.csv");
+	open_csv_and_make_professor(professors);
+	Menuselect(students, professors);
+	
+	//std::cout<<toHash("")
+	return 0;
+}
